3b54082a648c9a216af4c894ad6c9e888958a29f
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-07-01 09:03:34 -0700

    Refactor and clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 $(document).ready(() => {
   $(document).one("scroll", () => {$("#header").addClass("scrolled-header");});
-  $("#search-icon").hover(() => {$("#search-input").toggle(1500);});
+  $("#search-icon").hover(() => {$("#search-input").toggle(1000);});
   $('.owl-carousel').owlCarousel({
-    items : 1,
-    dotsEach : true,
+    items: 1,
+    dotsEach: true,
   });  
   
   $(".card-widget-item").hover(function() {
     $(this).find(".card-content").toggleClass("card-overlay");
     $(this).find(".card-widget-title, .card-widget-description, .more-button").toggle();
   });
-    
+  
+  $(".image-container").each(function(i) {
+    let imageClass = `image${i+1}`
+    $(this).addClass(`${imageClass}`);
+  });
+  
   $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data){
     let events = "";
-    let images = ["fun", "stage", "music", "guitar", "restaurant", "drinks", "cheers", "food", "nature", "friends"]
+    let images = ["fun", "stage", "music", "guitar", "restaurant", "drinks", "cheers", "food", "nature", "friends"];
     
     for (let i = 1; i <= 10; i++) {
       events += `<div class="event-item"><img src="https://loremflickr.com/500/500/${images[i]}" alt="${images[i]}"><div class="event-content"><h3>${limitTitle(data[i].title)}</h3><p>${limitBody(data[i].body)}</p></div></div>`
@@ -39,4 +44,3 @@ function limitBody(body) {
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
